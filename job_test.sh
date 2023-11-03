f792fe198b211cad185a6dc853170b7e96e580b3
Farrukh from cluster <dr.farrukh89@gmail.com>
2023-11-03 20:30:54 +0100

    cluster

--- job_test.sh
+++ job_test.sh
@@ -4,17 +4,19 @@
 #SBATCH --mem=60gb
 #SBATCH --time=12:00:00
 #SBATCH --job-name=Classification
-#SBATCH --error=Outputs/%j.err_
-#SBATCH --output=Outputs/%J.out_
+#SBATCH --error=jupyter_err.log
+#SBATCH --output=jupyter.log
 #SBATCH -p tesla
 #SBATCH -q tesla
 #SBATCH -A kdm-grant594
 #SBATCH --gres=gpu:tesla:1
 
-apptainer exec --nv tensorflow_latest-gpu.sif pip3 install -q kaggle --user
-apptainer exec --nv tensorflow_latest-gpu.sif pip3 install -q matplotlib seaborn scikit-learn imbalanced-learn --user
-apptainer exec --nv tensorflow_latest-gpu.sif pip3 install -q jupyterlab
-apptainer exec --nv tensorflow_latest-gpu.sif pip3 install -q jupyterlab --user
+apptainer exec --nv pytorch_latest.sif pip3 install -q kaggle --user
+apptainer exec --nv pytorch_latest.sif pip3 install -q matplotlib seaborn scikit-learn imbalanced-learn --user
+apptainer exec --nv pytorch_latest.sif pip3 install -q jupyterlab notebook
+apptainer exec --nv pytorch_latest.sif pip3 install -q jupyterlab notebook --user
+apptainer exec --nv pytorch_latest.sif export PATH="$PATH:/home/abbass12/.local/bin"
+apptainer exec --nv pytorch_latest.sif export PATH="$PATH:/root/.local/bin"
 
 
-apptainer exec --nv tensorflow_latest-gpu.sif jupyter lab --ip=0.0.0.0 --port=8888 --no-browser
+apptainer exec --nv pytorch_latest.sif jupyter lab --ip=0.0.0.0 --port=62094 --no-browser
